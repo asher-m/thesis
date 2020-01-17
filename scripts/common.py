95382eac95e2dd9b4465a967881760a13568f7a5
Asher Merrill <asher.s.merrill@gmail.com>
2020-01-16 19:03:22 -0500

    fitting: Exponent with power-law fitting param and UNIX line endings.

--- scripts/common.py
+++ scripts/common.py
@@ -14,7 +14,7 @@ def uncert_prop(inarr, axis):
     along that axis. """
     # Uncertainty carries like addition of quadrature divided by number of
     # things.
-    return numpy.sqrt(numpy.nansum(inarr**2, axis=axis)) / \
-        numpy.nansum(numpy.invert(numpy.isnan(inarr)), axis=axis)
+    return numpy.sqrt(numpy.nansum(inarr**2, axis=axis)) \
+        / numpy.sum(numpy.invert(numpy.isnan(inarr)), axis=axis)
     # Alternatively, we may need to use:
     # return numpy.sqrt(numpy.nansum(inarr**2, axis=axis))
--- scripts/ic_models.py
+++ scripts/ic_models.py
@@ -7,5 +7,17 @@ import numpy
 
 
 
-def fisk_2008_eq38(e, e_0, j_0):
-    return j_0 * e**-1.5 * numpy.exp(-e / e_0)
+def fisk_2008_eq38(e, e_0, e_b, j_0):
+    """
+    Fisk & Gloeckler 2008 Eq. 38
+    Note that I've added a translation parameter to the exponential.
+    """
+    return j_0 * e**-1.5 * numpy.exp(-(e - e_b) / e_0)
+
+def fisk_2008_eq38_modified(e, j_0, k):
+    """
+    Fisk & Gloeckler 2008 Eq. 38
+    Note that I've added a translation parameter to the exponential.
+    Changed a bit.
+    """
+    return j_0 * e**k
