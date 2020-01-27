666bc4235fea8984771288b5a1a152acadb31eb4
Asher Merrill <asher.s.merrill@gmail.com>
2020-01-26 23:01:07 -0500

    clickthrough: Can now walk through events in some given file to see what they look(ed) like.

--- scripts/ic_mean_concat_clickthrough.py
+++ scripts/ic_mean_concat_clickthrough.py
@@ -5,57 +5,97 @@ or epoch and display using spacepy.plot.utils.EventClicker so events can be
 identified.
 """
 
+import argparse
 import datetime
+import glob
 import matplotlib
 import matplotlib.cm
 import matplotlib.colors
 import matplotlib.pyplot as plt
 import numpy
+import os
+import os.path
 import pickle
+import re
 import sys
 
 import spacepy.plot
 
-energy = [
-    69.8,
-    84.5,
-    91.1,
-    98.0,
-    110,
-    132,
-    160,
-    198,
-    286,
-    493,
-    886,
-    1630,
-    3030,
-    5640,
-    8320,
-    ]
 
 
+def main(click=False, review=None):
+    """ Main function to generate plot of all files that can be found in
+    data dir.
 
-def main():
-    with open('../data/ic_event_datetime_flux.pickle{}'.format(sys.version_info[0]), 'rb') as fp:
-        arrs = pickle.load(fp)
+    click is a bool that triggers spacepy.plot.utils.EventClicker walk through
+    of event selection.
+    review is a string of the file to review.
 
-    # Flux:
-    g = arrs['flux']
-    # Datetime/epoch:
-    t = arrs['epoch']
-
+    These options cannot be used together. """
     # Get colormesh that we'll paint out nan's from:
     cmap = matplotlib.cm.get_cmap('jet')
     cmap.set_bad(color='black')
-    # cmap.set_bad(color='gray')
 
-    # Need transpose of g, because I guess that's how it always worked...
-    plt.pcolormesh(t, energy, g.T, cmap=cmap, norm=matplotlib.colors.LogNorm(), shading='flat', edgecolors='None', rasterized=True)
-    plt.yscale('log')
-    plt.ylim((10, 2000))
+    plotting_at = 0
+    # First things first we need to open the arrays and make the pcolormeshes.
+    for f in glob.glob('../data/ic_event*'):
+        m = re.match(r'^.*_(Chan[RPT]{{1}})_flux.pickle{}$'\
+                     .format(sys.version_info[0]),
+                     os.path.basename(f))
+        varname = m.group(1)
+        # Open the arrays:
+        with open(f, 'rb') as fp:
+            arrs = pickle.load(fp)
+
+        # Flux:
+        flux = arrs['flux']
+        # We don't need the flux delta:
+        # # Flux delta:
+        # dflux = arrs['dflux']
+        # Datetime/epoch:
+        epoch = arrs['epoch']
+        # Energy bins:
+        energy = arrs['energy']
+
+        # Need to flip all of these if we're working with ChanT:
+        if varname == 'ChanT':
+            flux = flux[:, ::-1]
+            # dflux = dflux[:, ::-1]
+            energy = energy[:, ::-1]
+
+        # We need to get the max size of array (because it changes when the
+        # binning table changes.  See 2018-09-03/05).
+        # np.argmax gets the index of the max value.
+        first_nonnan = numpy.where(~numpy.isnan(energy[numpy.argmax(numpy.sum(~numpy.isnan(energy), axis=1))]) == True)[0][0]
+        lenn = numpy.sum(~numpy.isnan(energy[numpy.argmax(numpy.sum(~numpy.isnan(energy), axis=1))]))
+        last_nonnan = first_nonnan + lenn
+
+        # Now we can finally plot these:
+        plt.pcolormesh(epoch,
+                       numpy.arange(plotting_at, last_nonnan - first_nonnan + plotting_at),
+                       flux[:, first_nonnan:last_nonnan].T,
+                       cmap=cmap,
+                       norm=matplotlib.colors.LogNorm(),
+                       shading='flat',
+                       edgecolors='None',
+                       rasterized=True)
+
+        # This essentially just shifts up the plot enough so that we can see
+        # each one when we review the events on one canvas.
+        # THIS IS NEEDED because EventClicker is made to work on one axis
+        # (matplotlib object) and doesn't yet work over multiple.
+        plotting_at += last_nonnan - first_nonnan + 5
+
     plt.colorbar()
+    plt.ylim((-5, plotting_at))
 
+    # Now we can call the review or clickthrough function:
+    if click is True:
+        clickthrough()
+    elif review is not None:
+        reviewthrough(review, plotting_at)
+
+def clickthrough():
     # This is preliminary, but might do max and half max..?
     c = spacepy.plot.utils.EventClicker(n_phases=2, interval=datetime.timedelta(days=30))
     c.analyze()
@@ -68,5 +108,42 @@ def main():
               .format(sys.version_info[0]), 'wb') as fp:
         pickle.dump(c.get_events(), fp)
 
+def reviewthrough(events_file, plotting_at):
+    with open(events_file, 'rb') as fp:
+        e = pickle.load(fp)
+        # Note that e is like: [((t0_start, y0_start), (t0_stop, y0_stop)),
+        #                       ((t1_start, y1_start), (t1_stop, y1_stop)), ...]
+
+    # Now, for each event (row) in e:
+    for i, event in enumerate(e):
+        startx = event[0, 0]
+        stopx = event[1, 0]
+        plt.vlines(startx, -5, plotting_at, color='red')
+        plt.vlines(stopx, -5, plotting_at, color='cyan')
+
+    plt.show()
+
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser()
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument('-c', help='enable script for clickthrough',
+                        dest='click',
+                        action='store_true')
+    group.add_argument('-r', help='enable script for event review',
+                        dest='review',
+                        action='store_true')
+    parser.add_argument(help='events definition file (from clickthrough)',
+                        dest='events_file',
+                        action='store',
+                        nargs='?')
+    args = parser.parse_args()
+    if args.review and not args.events_file:
+        parser.error('option -r requires events_file!')
+    if args.click is True:
+        main(click=True)
+    elif args.review is True:
+        main(review=args.events_file)
+    else:
+        # This shouldn't be able to happen:
+        raise RuntimeError('Must either reivew events or clickthrough events.'\
+                           '\nEither is required.')
