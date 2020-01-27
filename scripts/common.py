df31f36c5b2429fcf2772396b4914d7b67f0d770
Asher Merrill <asher.s.merrill@gmail.com>
2020-01-26 21:38:00 -0500

    fitting: Broke nan-trunc'ing into common from fit script, removed VAR by performing trunc lookup.
    * Also added the same argparse(r) to the fit script as from the mean and concat script so everything can be handled from the command line.
    * NaN trunc'ing is generic enough that it can be used elsewhere (for other arrays, not just the energy depend).

--- scripts/common.py
+++ scripts/common.py
@@ -9,23 +9,6 @@ import numpy
 
 
 
-# This can be ChanR, ChanT, or ChanP
-VAR = "ChanP"
-
-assert VAR in ['ChanT', 'ChanR', 'ChanP']
-
-# Fitting parameters:
-if VAR == 'ChanT':
-    FIT_TRUNK_LOWER = 37
-    FIT_TRUNK_UPPER = 400
-elif VAR == 'ChanR':
-    FIT_TRUNK_LOWER = 95
-    FIT_TRUNK_UPPER = 200
-elif VAR == 'ChanP':
-    FIT_TRUNK_LOWER = 95
-    FIT_TRUNK_UPPER = 305
-
-
 def uncert_prop(inarr, axis):
     """ Propagate the uncertainty of numbers on some axis when averaging down
     along that axis. """
@@ -35,3 +18,53 @@ def uncert_prop(inarr, axis):
         / numpy.sum(numpy.invert(numpy.isnan(inarr)), axis=axis)
     # Alternatively, we may need to use:
     # return numpy.sqrt(numpy.nansum(inarr**2, axis=axis))
+
+def nan_cut(inarr):
+    """ Check that all (non-NaN) elements of array are constant along zeroth
+    axis (0), then cuts out just the non-NaN elements from the first axis (1)
+    and hands them back along with the indices of the first and last non-NaN
+    elements so the same can be done to associated arrays.
+
+    Returns the input array cut down as described (or not, see return args),
+    and the indices to cut associated arrays in the same manner,
+    (indices of first and last non-NaNs along first axis (1)). """
+    # import pdb; pdb.set_trace()
+
+    # Make sure energy is consistent within this time range.
+    # This is a different condition than tried in the mean-concat script,
+    # because that checks within hours.  This checks between hours
+    # (and therefore, possibly files as well).
+    # ALSO, we don't need to index along look direction axis because we
+    # already checked in the mean-concat script.
+    assert numpy.all((inarr == inarr[0])[~numpy.isnan(inarr)])
+    # If this works (because we have the same binning in the file)
+    # we can just use the first set of not-all-NaNs bins
+    # (because they're all the same):
+    nonnan = numpy.where(numpy.any(~numpy.isnan(inarr), axis=1) \
+                         == True)[0][0]
+    inarr = inarr[nonnan]
+
+    # Lastly, we can count the number of non-NaNs we have, so we know where
+    # to stop the array trunking (so we don't try to plot NaNs):
+    lenn = numpy.sum(~numpy.isnan(inarr))
+    # And get the first not-nan so we know where to cut off the first few
+    # NaNs, (for ChanT, for example):
+    first_nonnan = numpy.where(~numpy.isnan(inarr) == True)[0][0]
+    last_nonnan = first_nonnan + lenn
+    inarr_cutdown = inarr[first_nonnan:last_nonnan]
+
+    return inarr, inarr_cutdown, first_nonnan, last_nonnan
+
+def energy_trunc(varname):
+    """ Function to return auto bounds on energy range for fits for each var
+    type.
+
+
+    Returns as (lower_bound, upper_bound)."""
+    # Fitting parameters:
+    if varname == 'ChanT':
+        return 37, 400
+    elif varname == 'ChanR':
+        return 95, 200
+    elif varname == 'ChanP':
+        return 95, 305
--- scripts/ic_fit.py
+++ scripts/ic_fit.py
@@ -12,7 +12,7 @@ import scipy.optimize
 import sys
 
 # This import is a bit ugly, but it's less ugly than retyping this every time:
-from common import uncert_prop, VAR, FIT_TRUNK_LOWER, FIT_TRUNK_UPPER
+from common import uncert_prop, nan_cut, energy_trunc
 from ic_models import fisk_2008_eq38_modified_centered as model
 
 # This is pretty case-specific right now, but I can blow it up to be more
@@ -28,10 +28,10 @@ YLIM_LOWER = 1e-3
 YLIM_UPPER = 10
 
 
-def main(events_file):
+def main(events_file, varname):
     # Open the arrays:
     with open('../data/ic_event_{}_flux.pickle{}'\
-              .format(VAR, sys.version_info[0]),
+              .format(varname, sys.version_info[0]),
               'rb') as fp:
         arrs = pickle.load(fp)
 
@@ -43,6 +43,9 @@ def main(events_file):
     epoch = arrs['epoch']
     # Energy bins:
     energy = arrs['energy']
+
+    # Also get the energy trunc's:
+    fit_lower_bound, fit_upper_bound = energy_trunc(varname)
 
     # Open the saved event times:
     with open(events_file, 'rb') as fp:
@@ -61,7 +64,7 @@ def main(events_file):
         cenergy = energy[startidx:stopidx]
 
         # Need to flip all of these if we're working with ChanT:
-        if VAR == 'ChanT':
+        if varname == 'ChanT':
             cflux = cflux[:, ::-1]
             cdflux = cdflux[:, ::-1]
             cenergy = cenergy[:, ::-1]
@@ -70,34 +73,15 @@ def main(events_file):
         cflux = numpy.nanmean(cflux, axis=0)
         cdflux = uncert_prop(cdflux, axis=0)
 
-        # import pdb; pdb.set_trace()
-
-        # Make sure energy is consistent within this time range.
-        # This is a different condition than tried in the mean-concat script,
-        # because that checks within hours.  This checks between hours
-        # (and therefore, possibly files as well).
-        # ALSO, we don't need to index along look direction axis because we
-        # already checked in the mean-concat script.
-        assert numpy.all((cenergy == cenergy[0, :])[~numpy.isnan(cenergy)])
-        # If this works (because we have the same binning in the file)
-        # we can just use the first set of not-all-NaNs bins
-        # (because they're all the same):
-        nonnan = numpy.where(numpy.any(~numpy.isnan(cenergy), axis=1) \
-                             == True)[0][0]
-        cenergy = cenergy[nonnan]
-
-        # Lastly, we can count the number of non-NaNs we have, so we know where
-        # to stop the array trunking (so we don't try to plot NaNs):
-        lenn = numpy.sum(~numpy.isnan(cenergy))
-        # And get the first not-nan so we know where to cut off the first few
-        # NaNs, (for ChanT, for example):
-        first_nonnan = numpy.where(~numpy.isnan(cenergy) == True)[0][0]
+        # Cut down the energy array to just the non-NaN elements (and find
+        # first/last indices of non-nan elements for the second axis).
+        cenergy, _, first_nonnan, last_nonnan = nan_cut(cenergy)
+        # Note: We're throwing away the pre-cutdown array because it makes
+        # it more explicit everywhere else.
 
         # THEN cut down to the energies we're interested in studying:
-        e_startidx = numpy.searchsorted(cenergy[first_nonnan:first_nonnan+lenn],
-                                        FIT_TRUNK_LOWER)
-        e_stopidx = numpy.searchsorted(cenergy[first_nonnan:first_nonnan+lenn],
-                                       FIT_TRUNK_UPPER)
+        e_startidx = numpy.searchsorted(cenergy[first_nonnan:last_nonnan], fit_lower_bound)
+        e_stopidx = numpy.searchsorted(cenergy[first_nonnan:last_nonnan], fit_upper_bound)
 
         # Set this up now in case the optimization doesn't fail:
         plt.figure(figsize=(10, 8))
@@ -106,9 +90,9 @@ def main(events_file):
             # We now have an array with 15 values, and an array with the uncertainties
             # in those values.  We should be able to fit this now.
             popt, pcov = scipy.optimize.curve_fit(model,
-                                                  cenergy[first_nonnan:first_nonnan+lenn][e_startidx:e_stopidx],
-                                                  cflux[first_nonnan:first_nonnan+lenn][e_startidx:e_stopidx],
-                                                  sigma=cdflux[first_nonnan:first_nonnan+lenn][e_startidx:e_stopidx],
+                                                  cenergy[first_nonnan:last_nonnan][e_startidx:e_stopidx],
+                                                  cflux[first_nonnan:last_nonnan][e_startidx:e_stopidx],
+                                                  sigma=cdflux[first_nonnan:last_nonnan][e_startidx:e_stopidx],
                                                   absolute_sigma=True)
             # I believe we DO in fact have absolute sigma, correct?  (See note
             # about this.)
@@ -127,14 +111,15 @@ def main(events_file):
 
         # And just plot it for now:
         # Plot the points used for fit:
-        plt.plot(cenergy[first_nonnan:first_nonnan+lenn][e_startidx:e_stopidx],
-                 cflux[first_nonnan:first_nonnan+lenn][e_startidx:e_stopidx],
+        plt.plot(cenergy[first_nonnan:last_nonnan][e_startidx:e_stopidx],
+                 cflux[first_nonnan:last_nonnan][e_startidx:e_stopidx],
                  'k.',
                  label='Points used for fit')
+
         # Don't cut down energies because we don't care about just displaying:
-        plt.errorbar(cenergy[first_nonnan:first_nonnan+lenn],
-                     cflux[first_nonnan:first_nonnan+lenn],
-                     yerr=cdflux[first_nonnan:first_nonnan+lenn],
+        plt.errorbar(cenergy[first_nonnan:last_nonnan],
+                     cflux[first_nonnan:last_nonnan],
+                     yerr=cdflux[first_nonnan:last_nonnan],
                      color='red')
 
         plt.xlim((XLIM_LOWER, XLIM_UPPER))
@@ -150,7 +135,7 @@ def main(events_file):
                    prop={'family':'monospace'})
 
         plt.title('{} Event: {} to {}'\
-                  .format(VAR,
+                  .format(varname,
                           event[0, 0].strftime('%F %H%M'),
                           event[1, 0].strftime('%F %H%M'),
                           )
@@ -158,7 +143,7 @@ def main(events_file):
 
         plt.tight_layout()
 
-        plt.savefig('../figures/spectrum_{}_{:02d}.png'.format(VAR, i),
+        plt.savefig('../figures/spectrum_{}_{:02d}.png'.format(varname, i),
                     dpi=300)
         plt.close()
 
@@ -168,5 +153,19 @@ if __name__ == "__main__":
     parser.add_argument(help='events definition file (from clickthrough)',
                         dest='events_file',
                         action='store')
+    parser.add_argument(help='name of the variable to fit, can be'
+                        ' \'ChanP\', \'ChanT\', \'ChanR\', or \'all\'',
+                        dest='varname',
+                        action='store')
     args = parser.parse_args()
-    main(args.events_file)
+    # Make sure we have something we can look at. If this assertion fails,
+    # it's because we received an invalid varname.
+    assert args.varname in ('ChanP', 'ChanT', 'ChanR', 'all')
+    if args.varname != 'all':
+        main(args.events_file, args.varname)
+    else:
+        for varname in ('ChanP', 'ChanT', 'ChanR'):
+            print('='*80)
+            print('{:^80}'.format('Starting {}...'.format(varname)))
+            print('='*80)
+            main(args.events_file, varname)
