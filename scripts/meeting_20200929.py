7ddfb39880acde2973344167a6a8f6dd3000ddd7
Asher Merrill <asher.s.merrill@gmail.com>
2020-10-05 21:11:54 -0400

    meeting: make spectral plots

--- scripts/meeting_20200929.py
+++ scripts/meeting_20200929.py
@@ -7,6 +7,7 @@ import matplotlib.colors
 import matplotlib.pyplot as plt
 import matplotlib.pyplot as plt
 import numpy
+import scipy.optimize
 
 import spacepy
 import spacepy.pycdf
@@ -15,7 +16,11 @@ import spacepy.datamanager
 import data
 
 
-def rebin(epoch, flux, pa, sa, cadence=None):
+FLUX_LIMLOW, FLUX_LIMHIGH = 1e-3, 1e4
+P0 = numpy.array([1e10, -3/2])
+
+
+def rebin(epoch, flux, pa, sa, cadence=None, flux_unc=None):
     """ Rebin fluxes. 
     
     Cadence in nanoseconds; default None averages entire range. """
@@ -24,6 +29,13 @@ def rebin(epoch, flux, pa, sa, cadence=None):
     lookdir_bins = numpy.linspace(0, 180, 4)
     flux_pa = spacepy.datamanager.rebin(flux, pa, lookdir_bins, axis=1)
     flux_sa = spacepy.datamanager.rebin(flux, sa, lookdir_bins, axis=1)
+    if flux_unc is not None:
+        flux_unc_omni = spacepy.datamanager.rebin(
+            flux_unc, sa, [0, 180], axis=1, bintype='unc')
+        flux_unc_pa = spacepy.datamanager.rebin(
+            flux_unc, pa, lookdir_bins, axis=1, bintype='unc')
+        flux_unc_sa = spacepy.datamanager.rebin(
+            flux_unc, sa, lookdir_bins, axis=1, bintype='unc')
 
     # Epoch: basically need hours or else it's too hard to plot
     if cadence is not None:
@@ -35,6 +47,13 @@ def rebin(epoch, flux, pa, sa, cadence=None):
     flux_omni = spacepy.datamanager.rebin(flux_omni, epoch, epoch_bins, axis=0)
     flux_pa = spacepy.datamanager.rebin(flux_pa, epoch, epoch_bins, axis=0)
     flux_sa = spacepy.datamanager.rebin(flux_sa, epoch, epoch_bins, axis=0)
+    if flux_unc is not None:
+        flux_unc_omni = spacepy.datamanager.rebin(
+            flux_unc_omni, epoch, epoch_bins, axis=0, bintype='unc')
+        flux_unc_pa = spacepy.datamanager.rebin(
+            flux_unc_pa, epoch, epoch_bins, axis=0, bintype='unc')
+        flux_unc_sa = spacepy.datamanager.rebin(
+            flux_unc_sa, epoch, epoch_bins, axis=0, bintype='unc')
 
     # Now break into appropriate stuff:
     flux_omni = numpy.squeeze(flux_omni, axis=1)
@@ -48,6 +67,21 @@ def rebin(epoch, flux, pa, sa, cadence=None):
         'perp': flux_sa[:, 1],
         'apar': flux_sa[:, 2]
     }
+
+    if flux_unc is not None:
+        flux_unc_omni = numpy.squeeze(flux_unc_omni, axis=1)
+        flux_unc_pa = {
+            'par': flux_unc_pa[:, 0],
+            'perp': flux_unc_pa[:, 1],
+            'apar': flux_unc_pa[:, 2]
+        }
+        flux_unc_sa = {
+            'par': flux_unc_sa[:, 0],
+            'perp': flux_unc_sa[:, 1],
+            'apar': flux_unc_sa[:, 2]
+        }
+        return epoch_fake, flux_omni, flux_pa, flux_sa, flux_unc_omni, flux_unc_pa, flux_unc_sa
+    else:
     return epoch_fake, flux_omni, flux_pa, flux_sa
 
 
@@ -59,7 +93,7 @@ def spectrogram(epoch, flux_omni, flux_pa, flux_sa, energy, pname, keepfig=False
     cmap = matplotlib.cm.get_cmap('jet')
     cmap.set_bad(color='black')
     # log norm
-    norm = matplotlib.colors.LogNorm(vmin=1e-4, vmax=1e6)
+    norm = matplotlib.colors.LogNorm(vmin=FLUX_LIMLOW, vmax=FLUX_LIMHIGH)
 
     fig, axes = plt.subplots(nrows=3, ncols=3,
                              figsize=(20, 20),
@@ -128,6 +162,107 @@ def spectrogram(epoch, flux_omni, flux_pa, flux_sa, energy, pname, keepfig=False
         plt.close(fig)
 
 
+def spectrum(epoch, flux_omni, flux_unc_omni, flux_pa, flux_unc_pa, flux_sa, flux_unc_sa, energy, pname, keepfig=False):
+    """ Make a sprectrogram for one event.
+
+    flux_pa and flux_sa are dicts keyed by 'par', 'apar', 'perp'. """
+    def label_axis(ax):
+        ax.set_yscale('log')
+        ax.set_ylabel('j')
+        ax.set_ylim(FLUX_LIMLOW, FLUX_LIMHIGH)
+        ax.set_xscale('log')
+        ax.set_xlabel('Energy (keV)')
+        ax.set_xlim(20, 200)
+
+    energy_bounds = ((37, 160), (95, 160))
+
+    fig, axes = plt.subplots(nrows=3, ncols=3,
+                             figsize=(20, 20),
+                             sharex=True, sharey=True)
+
+    # Check to make sure energy is consistent across time:
+    assert(numpy.all(energy == energy[0]))
+
+    erange = numpy.linspace(energy[0, 0, 0], energy[0, 0, -1])
+
+    # Sharing x and y, so set them smartly:
+    label_axis(axes[0, 0])
+
+    for j, eset in enumerate(energy_bounds):
+        elower = numpy.searchsorted(energy[0, 0], eset[0])
+        eupper = numpy.searchsorted(energy[0, 0], eset[1])
+
+        print(j, ';', eset, ':', elower, eupper)
+        # import pdb; pdb.set_trace()
+
+
+        # First plot omni:
+        ax = axes[0, 1]
+        popt, pcov = scipy.optimize.curve_fit(
+            data.model,
+            energy[0, 0, elower:eupper],
+            flux_omni[0, elower:eupper],
+            sigma=flux_unc_omni[0, elower:eupper],
+            absolute_sigma=True,
+            p0=P0
+        )
+        ax.plot(erange, data.model(erange, *popt))
+        if j == 0:  # if first time around do the plot:
+            ax.errorbar(
+                energy[0, 0],
+                flux_omni[0],
+                yerr=flux_unc_omni[0]
+            )
+        print(j, ';', eset, ':', *popt)
+
+        # Now plot pa:
+        for i, k in enumerate(('par', 'perp', 'apar')):
+            ax = axes[1, i]
+            popt, pcov = scipy.optimize.curve_fit(
+                data.model,
+                energy[0, 0, elower:eupper],
+                flux_pa[k][0, elower:eupper],
+                sigma=flux_unc_pa[k][0, elower:eupper],
+                absolute_sigma=True,
+                p0=P0
+            )
+            ax.plot(erange, data.model(erange, *popt))
+            if j == 0:
+                ax.errorbar(
+                    energy[0, 0],
+                    flux_pa[k][0],
+                    yerr=flux_unc_pa[k][0]
+                )
+
+        # Now plot sa:
+        for i, k in enumerate(('par', 'perp', 'apar')):
+            ax = axes[2, i]
+            popt, pcov = scipy.optimize.curve_fit(
+                data.model,
+                energy[0, 0, elower:eupper],
+                flux_sa[k][0, elower:eupper],
+                sigma=flux_unc_sa[k][0, elower:eupper],
+                absolute_sigma=True,
+                p0=P0
+            )
+            ax.plot(erange, data.model(erange, *popt))
+            if j == 0:
+                ax.errorbar(
+                    energy[0, 0],
+                    flux_sa[k][0],
+                    yerr=flux_unc_sa[k][0]
+                )
+
+    # Save:
+    fig.tight_layout()
+    fig.savefig(pname)
+
+    if keepfig is not False:
+        return fig, axes
+    else:
+        plt.close(fig)
+
+
 def main():
     eventdata = data.read_data(verbose=True)
     for i, e in enumerate(eventdata):
@@ -144,26 +279,29 @@ def main():
                 if not len(epoch) > 1:
                     continue
 
-                epoch_fake, flux_omni, flux_pa, flux_sa = rebin(
+
+                epoch_fake, flux_omni, flux_pa, flux_sa, flux_unc_omni, flux_unc_pa, flux_unc_sa = rebin(
                     epoch,
                     flux,
                     pa,
                     sa,
-                    cadence=60 * 60e9  # 1 hour in nanoseconds
+                    flux_unc=flux_unc
                 )
             
-                spectrogram(
+                spectrum(
                     epoch_fake,
                     flux_omni,
+                    flux_unc_omni,
                     flux_pa,
+                    flux_unc_pa,
                     flux_sa, 
+                    flux_unc_sa,
                     energy, 
-                    'meeting_20200929/spectrogram_event-{:02d}_{}_{}.png'.format(
+                    'meeting_20200929/spectrum_event-{:02d}_{}_{}.png'.format(
                         i,
                         spacepy.pycdf.lib.tt2000_to_datetime(epoch[0]).strftime('%Y-%j'),
                         g['flux'].lower())
                 )
-
 
     return eventdata
 
