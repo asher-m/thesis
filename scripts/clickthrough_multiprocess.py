96bba314e8fd04c187a6bfb31676035a1ced7c5e
Asher Merrill <asher.s.merrill@gmail.com>
2021-01-25 18:31:02 -0500

    clickthrough: use spacepy.pycdf.istp.nan_fill

--- scripts/clickthrough_multiprocess.py
+++ scripts/clickthrough_multiprocess.py
@@ -20,56 +20,66 @@ if platform.system() == 'Linux':  # probably, hopefully running on isoc
     import isois.isois_cdf
     import isois.level3_master
 
+channels = (
+    'ChanT',
+    'ChanP',
+    # 'ChanR'  # makes things take forever because of memory limits
+)
+
 
 def process_file(f):
     print('Starting file {}...'.format(os.path.basename(f)))
-    r = dict()
-
     finfo = isois.isois_cdf.cdf_filename_parser(f)
     c = spacepy.pycdf.CDF(f)  # open cdf
+    r = dict()
+    r['epoch'] = edges(finfo['date'], epoch=True)
 
-    for v in ('ChanT', 'ChanP', 'ChanR'):
+    for v in channels:
         r[v] = dict()
 
         epoch = c.raw_var('Epoch_{}'.format(v))[...]
-        flux = c['H_Flux_{}'.format(v)][...]
-        energy = c['H_{}_Energy'.format(v)][...]
-        pa = c['PA_{}'.format(v)][...]
-
-        flux[flux < 0] = numpy.nan
-        energy[energy < 0] = numpy.nan
-        pa[pa < 0] = numpy.nan
+        # ...don't know how to check to make sure there isn't fill in here...
+        # get relevant vars
+        f_copy = c['H_Flux_{}'.format(v)].copy()
+        e_copy = c['H_{}_Energy'.format(v)].copy()
+        p_copy = c['PA_{}'.format(v)].copy()
+        # fill
+        spacepy.pycdf.istp.nanfill(f_copy)
+        spacepy.pycdf.istp.nanfill(e_copy)
+        spacepy.pycdf.istp.nanfill(p_copy)
+        # read plain data
+        f = f_copy[...]
+        e = e_copy[...]
+        p = p_copy[...]
 
         # get edges
         edges_times, edges_lookdir, edges_energy = edges(finfo['date'])
 
         # do energy rebin
-        flux = spacepy.datamanager.rebin(
-            flux,
-            energy,
+        f = spacepy.datamanager.rebin(
+            f,
+            e,
             edges_energy,
             axis=2
         )
         # do pa rebin
-        flux = spacepy.datamanager.rebin(
-            flux,
-            pa,
+        f = spacepy.datamanager.rebin(
+            f,
+            p,
             edges_lookdir['omni'],
             axis=1
         )
         # do time rebin
-        flux = spacepy.datamanager.rebin(
-            flux,
+        f = spacepy.datamanager.rebin(
+            f,
             epoch,
             edges_times,
             axis=0
         )
 
-        r[v]['flux'] = flux
-        r[v]['epoch'] = edges(finfo['date'], epoch=True)
+        r[v]['flux'] = f
 
     c.close()
-
     return r
 
 
@@ -82,13 +92,14 @@ def edges(datestr, epoch=False):
 
     Follows ordering of CDF depends. 
 
-    :param bool epoch: Yield epoch from calcualting time edges for CDF.    
+    :param str datestr: datestr of day
+    :param bool epoch: yield epoch from calcualting time edges for CDF
     """
     # Time bins: keep in mind this is an hourly product.
     today = datetime.datetime.strptime(datestr, '%Y%m%d')
     strt_tt2000 = spacepy.pycdf.lib.datetime_to_tt2000(today)
     stop_tt2000 = spacepy.pycdf.lib.datetime_to_tt2000(
-        today + datetime.timedelta(days=1))
+        today + datetime.timedelta(days=1))  # weirdness for leapseconds
     # Minutes, the "plus-minus value" (so half bin spacing)
     delta_minutes = 30
     delta = delta_minutes * 60e9  # convert to nanoseconds
@@ -97,12 +108,7 @@ def edges(datestr, epoch=False):
     # If flagged for epoch, return now:
     if epoch is True:
         return times
-    # By explicitly making the upper edge of the last bin the stop_tt2000 value
-    # the leap second is captured in the last bin.  This is in contrast
-    # to making the edges by adding the delta to all the values which risks
-    # not capturing leap seconds in the last bin.
-    edges_times = numpy.array(
-        list(times - delta) + list((stop_tt2000,)))
+    edges_times = numpy.array(list(times - delta) + list((stop_tt2000,)))
 
     # PA bins, for now:
     edges_lookdir = dict()
@@ -125,8 +131,8 @@ def edges(datestr, epoch=False):
 def read_data():
     # multiprocess stuff
     cpus = mp.cpu_count()
-    # cpus_to_use = cpus - 4 if cpus - 4 > 0 else cpus
-    cpus_to_use = 4
+    cpus_to_use = cpus - 4 if cpus - 4 > 0 else cpus
+    # cpus_to_use = 4
 
     # place to store outdata
     outdata = dict()
@@ -139,40 +145,28 @@ def read_data():
     # get files
     files = isois.get_latest('psp_isois-epilo_l2-ic')
     # some test cases:
-    # files = isois.get_latest('psp_isois-epilo_l2-ic')[:1]
     # files = isois.get_latest('psp_isois-epilo_l2-ic')[:10] # just first 10
-    # files = isois.get_latest('psp_isois-epilo_l2-ic')[:100]
+    # files = isois.get_latest('psp_isois-epilo_l2-ic', date='20190404')  # really big day
 
-    filesets = [files[i*cpus_to_use:(i+1)*cpus_to_use]
-                for i in range(int(len(files)//cpus_to_use+1))]
-    print('Total filesets {}...'.format(len(filesets)))
 
-    for v in ('ChanT', 'ChanP', 'ChanR'):
+    outdata['epoch'] = numpy.empty((0), dtype=numpy.int64)
+    outdata['epoch'].fill(-1)
+    for v in channels:
         # just create holding places
         outdata[v] = dict()
         outdata[v]['flux'] = numpy.empty((0, 32), dtype=numpy.float32)
         outdata[v]['flux'].fill(numpy.nan)
-        outdata[v]['epoch'] = numpy.empty((0), dtype=numpy.int64)
-        outdata[v]['epoch'].fill(-1)
 
     with mp.Pool(cpus_to_use) as pool:
-        for nn, fset in enumerate(filesets):
-            print('Starting fileset {:>3d}...'.format(nn))
-            data = pool.map(process_file, fset)
+        data = pool.map(process_file, files)
 
-            print('Adding fileset {:>3d} to outdata...'.format(nn))
-            for d in data:
-                for v in ('ChanT', 'ChanP', 'ChanR'):
-                    outdata[v]['flux'] = numpy.concatenate(
-                        (outdata[v]['flux'], numpy.squeeze(d[v]['flux'])))
-                    outdata[v]['epoch'] = numpy.concatenate(
-                        (outdata[v]['epoch'], d[v]['epoch']))
+    print('Concatenating data...')
+    for d in data:
+        outdata['epoch'] = numpy.concatenate((outdata['epoch'], d['epoch']))
+        for v in channels:
+            outdata[v]['flux'] = numpy.concatenate(
+                (outdata[v]['flux'], numpy.squeeze(d[v]['flux'])))
 
-    # for v in ('ChanT', 'ChanP', 'ChanR'):
-    #     for j, d in enumerate(data):
-    #         print('Storing file {}: {}...'.format(j, v))
-    #         outdata[v]['flux'][j*24:(j+1)*24] = numpy.squeeze(d[v]['flux'])
-    #         outdata[v]['epoch'][j*24:(j+1)*24] = d[v]['epoch']
 
     print('Zipping...')
     with bz2.BZ2File('../data/clickdata-multiprocess.pickle{}.bz2'.format(sys.version_info[0]),
